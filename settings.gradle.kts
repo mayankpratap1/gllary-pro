efa83d239f4fb2c981dbd676bc43cb194de4c210
EdgeLLM Builder <build@edgellm.pro>
2026-05-16 20:00:10 +0530

    Add JetBrains Space repositories and try KSP 2.3.0-1.0.22

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,8 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev") }
+        maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap") }
     }
 }
 dependencyResolutionManagement {
@@ -10,6 +12,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev") }
+        maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap") }
     }
 }
 
